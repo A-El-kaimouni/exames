eb3ba75f3125b2f000d931395934fd877ead951c
A_El_Kaimouni <abdrahmanelkaimouni2@gmail.com>
2021-06-06 02:11:53 +0100

    up

--- ts/helpers/exams.ts
+++ ts/helpers/exams.ts
@@ -4,7 +4,7 @@ export const exams = {
             exam: 'https://drive.google.com/file/d/1g_LXN9jtbzXbUwTPzi03F9lyDvsSA6t5/preview',
             corr: 'https://drive.google.com/file/d/1xuFUuNQgX6rX2KIo-k0jksYMNwL7lZP-/preview'
         },
-        duration: 1.5,
+        duration: 1,
         module: 'Analyse III'
     }
 }
